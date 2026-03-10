5168df67c7787952808948fe66f05690a7a78bef
nashmaher <7fzprjmtch@privaterelay.appleid.com>
2026-03-10 02:54:31 -0500

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,4 @@
-const Stripe = require("stripe");
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
@@ -157,7 +157,7 @@ function buildAdminEmail({ customerName, customerEmail, customerPhone, shippingA
 </table></td></tr></table></body></html>`;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
   try {
     const event = req.body;
