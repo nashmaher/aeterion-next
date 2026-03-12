4b51f39378566ba5d311238c788351576c8ea6ab
nashmaher <7fzprjmtch@privaterelay.appleid.com>
2026-03-12 12:58:24 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,10 @@
-import Head from 'next/head'
-import Script from 'next/script'
+// pages/_app.js
+import { CartProvider } from '../lib/CartContext';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        {/* Google Fonts – previously in index.html */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://kafwkhbzdtpsxkufmkmm.supabase.co" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://res.cloudinary.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="apple-mobile-web-app-title" content="Aeterion" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </Head>
-
-      {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-HP43H7V7T9"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-HP43H7V7T9');
-        `}
-      </Script>
-
+    <CartProvider>
       <Component {...pageProps} />
-    </>
-  )
+    </CartProvider>
+  );
 }
